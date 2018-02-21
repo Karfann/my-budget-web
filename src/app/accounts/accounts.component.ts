d741f39fcd7570cf38529a2508971b8928a8404b
Karfann Nacif <karfannacif@gmail.com>
2018-02-21 22:48:12 +0000

    Implemented accounts

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Account } from './../models/account';
 
 @Component({
   selector: 'app-accounts',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accounts.component.css']
 })
 export class AccountsComponent implements OnInit {
+
+  accounts: Account[] = [
+        new Account(1, 'Bco Abc', 0.00, true),
+        new Account(2, 'Cartao 1', 100.00, true),
+        new Account(3, 'Bco Zxy', 0.00, false)
+  ];
 
   constructor() { }
 
--- src/app/models/account.ts
+++ src/app/models/account.ts
@@ -1,2 +1,8 @@
 export class Account {
+    constructor(
+            private id:number,    
+            private name:string, 
+            private initialValue: number,
+            private isActive: boolean
+         ){}
 }
