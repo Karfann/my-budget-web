60784467c3129e7e4466d1f7819ccae75993ee1c
Karfann Nacif <karfannacif@gmail.com>
2018-03-07 22:52:00 +0000

    Created message module and component

---
+++ src/app/message/message.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MessageComponent } from './message/message.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MessageComponent],
+  providers: []
+})
+export class MessageModule { }
