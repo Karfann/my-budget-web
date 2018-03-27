7a08be35dde47ad17b33ced10ded0711a1e9ee24
Karfann Nacif <karfannacif@gmail.com>
2018-03-27 22:25:26 +0100

    Implemented category new component

--- src/app/categories/category-new/category-new.component.ts
+++ src/app/categories/category-new/category-new.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { Category } from '../shared/category';
+import { CategoryService } from '../shared/category.service';
+import { AlertService } from '../../shared/services/alert.service';
 
 @Component({
   selector: 'app-category-new',
@@ -7,9 +14,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryNewComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private categoryService: CategoryService,
+    private fb: FormBuilder,
+    private location: Location,
+    private router: Router,
+    private alertService: AlertService
+  ) { }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  onSubmit(): void {
+    const newCategory = this.prepareSave();
+    this.categoryService.addCategory(newCategory)
+      .subscribe(cat => {
+        this.alertService.success(`${cat.name} has been created with success`, true);
+        this.router.navigate(['/categories']);
+      });
+  }
+
+  reset(): void {
+    this.form.reset();
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  private createForm(): void {
+    this.form = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      isActive: ['', Validators.required]
+    });
+    this.form.controls['isActive'].setValue(true, { onlySelf: true });
+  }
+
+  private prepareSave(): Category {
+    const formModel = this.form.value;
+    const save: Category = {
+      id: 0,
+      name: formModel.name as string,
+      isActive: formModel.isActive as boolean
+    };
+    return save;
   }
 
 }
