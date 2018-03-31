108eb165225171d0694a0a8af9a3dcb1a83acd1f
Karfann Nacif <karfannacif@gmail.com>
2018-03-31 13:17:18 +0100

    Implemented type new component

--- src/app/types/type-new/type-new.component.ts
+++ src/app/types/type-new/type-new.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AlertService } from '../../shared/services/alert.service';
+import { TypeService } from '../shared/type.service';
+import { Type } from '../shared/type';
 
 @Component({
   selector: 'app-type-new',
@@ -7,9 +14,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TypeNewComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private typeService: TypeService,
+    private fb: FormBuilder,
+    private location: Location,
+    private router: Router,
+    private alertService: AlertService
+  ) { }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  onSubmit(): void {
+    const newType = this.prepareSave();
+    this.typeService.addType(newType)
+      .subscribe(sub => {
+        this.alertService.success(`${sub.name} has been created with success`, true);
+        this.router.navigate(['/types']);
+      });
+  }
+
+  reset(): void {
+    this.form.reset();
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  private createForm(): void {
+    this.form = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      isActive: [true, Validators.required],
+      value: [1, Validators.required]
+    });
+  }
+
+  private prepareSave(): Type {
+    const formModel = this.form.value;
+    const save: Type = {
+      id: 0,
+      name: formModel.name as string,
+      isActive: formModel.isActive as boolean,
+      value: formModel.value as number
+    };
+    return save;
   }
 
 }
